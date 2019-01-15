c883603b0bfea667a6e0a0f4c3ce639a422f0c2b
Nada Hachem <46658614+NadaHachem1980@users.noreply.github.com>
2019-01-15 11:28:59 -0500

    Add files via upload

---
+++ introduction-to-php.php
@@ -0,0 +1,89 @@
+<?php
+/*
++---+
+| 1 | 
++---+
+Print the sentence: Welcome to PHP!
+*/
+
+
+echo "welcome to PHP";
+
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/* 
++---+
+| 2 | 
++---+
+Print the sentence: PHP stands for "Hypertext Preprocessor"!
+*/
+
+echo "PHP stands for \"Hypertext Preprocessor\"";
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/* 
++---+
+| 3 | 
++---+
+Print the following sentence as a paragraph: PHP stands for "Hypertext Preprocessor"!
+*/
+
+
+echo "<p> PhP stands for \"Hypertext Preprocessor</p>\"";
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/* 
++---+
+| 4 | 
++---+
+Print the following acronyms - every acronym in a new line:
+
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/
+ 
+echo "HTML - Stands for Hypertext Markup Language";
+echo "CSS - Stands for Cascading Stylesheet";
+echo "JS - Stands for JavaScript" ;
+echo "PHP - Stands for Hypertext Preprocessor";
+echo "SQL - Stands for Structural Query Language";
+   
+   
+   
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/* 
++---+
+| 5 | 
++---+
+Print the following acronyms in unordered list - <ul> ... </ul>:
+
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/
+
+echo "<ul>
+<li>HTMl - Stands for Heypertext Markup Langauge</li>
+<li>CSS - Stands for Cascading Stylesheet</li>
+<li>JS - Stands for JavaScript</li>
+<li>PHP - Stands for Hypertext Preprocessor</li>
+<li>SQL - Stands for Structural Query Language</li>
+
+</ul>";
+
+?>
